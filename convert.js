afa6df0ab15f26f76ebdc2eb1ab68adbd9e35e0e
MALTIBWEH MANKILIK <maltibwehmankilik@gmail.com>
2026-04-07 01:25:44 +0100

    fix 2_4

--- convert.js
+++ convert.js
@@ -3,13 +3,17 @@ const data = require('./realistic_774_polling_units.json');
 
 let sql = "INSERT INTO polling_units (name, code, lga_id, capacity) VALUES\n";
 
-const values = data.map(pu => {
-    // Escaping single quotes in names (e.g., Jama'are)
+const values = data.map((pu, index) => {
     const safeName = pu.name.replace(/'/g, "''");
-    return `('${safeName}', '${pu.code}', ${pu.lgaId}, ${pu.capacity})`;
+
+    // THE FIX: Force the code to be globally unique by appending the index
+    // Example: "AB/AB/001-0", "AB/AB/001-1", etc.
+    const uniqueCode = `${pu.code}-${index}`;
+
+    return `('${safeName}', '${uniqueCode}', ${pu.lgaId}, ${pu.capacity})`;
 });
 
 sql += values.join(",\n") + ";\n";
 
 fs.writeFileSync('V12__Seed_Polling_Units.sql', sql);
-console.log("SQL file generated successfully!");
+console.log("SQL file generated successfully with unique codes!");
